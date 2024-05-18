764f79c72d3f88e1cd539323497b4430d6f2ae8b
hly-22 <s3979290@rmit.edu.vn>
2024-05-18 10:51:33 +0700

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,27 +4,7 @@ import UserManagement.LoginManager;
 import java.sql.SQLException;
 
 public class Main {
-//    public static void main(String[] args) throws SQLException {
-//        DatabaseConnection databaseConnection = new DatabaseConnection("jdbc:postgresql://localhost:5432/postgres", "lyminhhanh", null);
-//        databaseConnection.connect();
-//        System.out.println("Database Connected.");
-//
-////        SystemAdminView adminView = new SystemAdminView();
-////        adminView.displayAdminMenu();
-//
-////        PolicyOwnerView policyOwnerView = new PolicyOwnerView();
-////        policyOwnerView.displayPolicyOwnerMenu();
-//
-////        PolicyHolderView policyHolderView = new PolicyHolderView();
-////        policyHolderView.displayPolicyHolderMenu();
-//
-//        Login login = new Login(new UserAuthentication(databaseConnection), new SessionManager());
-//        login.displayLogin();
-//
-//
-//        databaseConnection.closeConnection();
-//        System.out.println("Database Closed.");
-//    }
+
     public static void main(String[] args) throws SQLException {
         DatabaseConnection databaseConnection = new DatabaseConnection("jdbc:postgresql://localhost:5432/postgres", "lyminhhanh", null);
         databaseConnection.connect();
@@ -33,6 +13,10 @@ public class Main {
         System.out.println("==== Welcome! ====");
         LoginManager loginManager = new LoginManager(databaseConnection);
         loginManager.runLogin();
+
+//        SystemAdmin systemAdmin = new SystemAdmin("admin", "12345", null);
+//        SystemAdminView adminView = new SystemAdminView(systemAdmin);
+//        adminView.displayAdminMenu();
 
         databaseConnection.closeConnection();
         System.out.println("Database Closed.");
